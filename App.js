ff021f4a3dfaf7462805f2a488a62cc7b63e3585
Phuc Ho <hotrongphuc643@gmail.com>
2024-04-30 00:28:02 +0700

    first commit

--- App.js
+++ App.js
@@ -5,10 +5,13 @@ import { useFonts } from 'expo-font'
 import OnboardingScreen from './screens/OnboardingScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import Transaction from './screens/Transaction/Transaction';
+import { Text, TouchableOpacity } from 'react-native';
+import AntDesign from "react-native-vector-icons/AntDesign"
 
 const Stack = createNativeStackNavigator();
 
-function App(){
+function App() {
     const [fontLoaded, fontError] = useFonts({
         'Inter-SemiBold': require('./assets/fonts/Inter-SemiBold.ttf'),
     });
@@ -17,6 +20,18 @@ function App(){
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen
+                    options={{
+                        headerTitleAlign: "center",
+                        headerLeft: () => (
+                            <TouchableOpacity>
+                                <AntDesign name="arrowleft" style={{ fontSize: 24 }} />
+                            </TouchableOpacity>
+                        ),
+                    }}
+                    name="Transaction"
+                    component={Transaction}
+                />
+                {/* <Stack.Screen
                     options={{ headerShown: false }}
                     name="OnboardingScreen"
                     component={OnboardingScreen}
@@ -29,10 +44,10 @@ function App(){
                 />
 
                 <Stack.Screen
-                    options={{ headerShown: false}}
+                    options={{ headerShown: false }}
                     name="RegisterScreen"
                     component={RegisterScreen}
-                />
+                /> */}
             </Stack.Navigator>
         </NavigationContainer>
     )
